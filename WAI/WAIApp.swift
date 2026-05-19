e0cfe176743fe009ca0c1a85b991ebf2f88467df
Jopepo <80066512+Jopepo@users.noreply.github.com>
2026-05-19 14:30:30 +0100

    Initial Commit

---
+++ WAI/WAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WAIApp.swift
+//  WAI
+//
+//  Created by Jopepo on 19/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct WAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WAITests/WAITests.swift
@@ -0,0 +1,17 @@
+//
+//  WAITests.swift
+//  WAITests
+//
+//  Created by Jopepo on 19/05/2026.
+//
+
+import Testing
+@testable import WAI
+
+struct WAITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
